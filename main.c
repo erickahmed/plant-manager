577499cccc3ed97da489568c382bd9c4d44af8ca
Erick Ahmed <engineering@erickahmed.com>
2025-05-01 22:01:14 +0200

    Implement 8 seconds watchdog

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
+#include <avr/sleep.h>
+
 #define MOISTURE_SENSOR_A0 A0
+
+volatile bool readSensors = false;
+
+ISR(WDT_vect) {
+    readSensorsPtr = true;
+}
+
+void watchdog() {
+    cli();
+    // Set watchdog timer to 8 seconds
+    WDTCSR = (1 << WDCE) | (1 << WDE);
+    WDTCSR = (1 << WDIE) | (1 << WDP3);
+    sei();
+}
 
 void setup() {
     pinMode(MOISTURE_SENSOR_A0, INPUT);
@@ -6,6 +22,13 @@ void setup() {
 }
 
 void loop() {
-    analogRead(MOISTURE_SENSOR_A0);
-    
+    if (readSensorsPtr) {
+        uint16_t moisture = analogRead(MOISTURE_SENSOR_A0);
+        Serial.println(moisture);
+
+        readSensorsPtr = false;
+    }
+
+
+
 }
