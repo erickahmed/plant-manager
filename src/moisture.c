bf7d177046f73524055d7b98d833a2be7c6f917a
Erick Ahmed <engineering@erickahmed.com>
2025-05-02 14:40:58 +0200

    Add Makefile and refactor moisture reading to separate module

---
+++ src/moisture.c
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <stdint.h>
+#include "moisture.h"
+#include "config.h"
+
+static int16_t moistureReadingSum = 0;
+static int8_t moistureReadingCount = 0;
+
+volatile bool readSensors = false;
+
+void initMoistureSensor(void) {
+    pinMode(MOISTURE_SENSOR_A0, INPUT);
+}
+
+void triggerSensorRead(void) {
+    readSensors = true;
+}
+
+int16_t readMoisture(void) {
+    if (!readSensors)
+        return -1;
+
+    moistureReadingSum += analogRead(MOISTURE_SENSOR_A0);
+    moistureReadingCount++;
+
+    if (moistureReadingCount >= SENSOR_READINGS) {
+        int16_t avg = moistureReadingSum / SENSOR_READINGS;
+        Serial.println(avg);
+        moistureReadingSum = 0;
+        moistureReadingCount = 0;
+        readSensors = false;
+        return avg;
+    }
+
+    readSensors = true;
+    return -1;
+}
