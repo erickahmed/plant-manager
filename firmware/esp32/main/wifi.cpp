b54d3c744c40e0a29cf16c46aec3c7c09545b2f2
Erick Ahmed <engineering@erickahmed.com>
2025-10-24 22:04:04 +0200

    Update libraries

--- firmware/esp32/main/wifi.cpp
+++ firmware/esp32/main/wifi.cpp
@@ -1,14 +1,8 @@
-#include <stdio.h>
-#include <string.h>
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "esp_system.h"
-#include "esp_log.h"
-#include "esp_task_wdt.h"
+#include "freertos/event_groups.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
-#include "esp_netif.h"
+#include "esp_log.h"
 #include "nvs_flash.h"
 #include "main.hpp"
 #include "wifi-credentials.h"
