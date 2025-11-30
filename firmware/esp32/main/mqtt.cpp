3670b58f8e53a616bedfb79368294eb107f9329c
Erick Ahmed <engineering@erickahmed.com>
2025-11-30 23:36:54 +0100

    Implement task handle for managing I2C

--- firmware/esp32/main/mqtt.cpp
+++ firmware/esp32/main/mqtt.cpp
@@ -12,18 +12,36 @@
 
 static const char* TAG = "MQTT";
 
+static TaskHandle_t i2c_task_handle = NULL;
+static esp_mqtt_client_handle_t mqtt_client = NULL;
 const int MQTT_CONNECTED_BIT = BIT0;
+
+static void mqtt_subscribe(void) {
+    msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+}
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data) {
     esp_mqtt_event_handle_t event = event_data;
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED:
             xEventGroupSetBits(connectivity_event_group, MQTT_CONNECTED_BIT);
+            mqtt_subscribe();
             ESP_LOGI(TAG, "Connected to broker");
             break;
         case MQTT_EVENT_DISCONNECTED:
             xEventGroupClearBits(connectivity_event_group, MQTT_CONNECTED_BIT);
-            ESP_LOGI(TAG, "Disconnected from broker");
+            ESP_LOGW(TAG, "Disconnected from broker");
+            break;
+        case MQTT_EVENT_DATA:
+            ESP_LOGI(TAG, "Listening to broker");
+            ESP_LOGV(TAG, "Topic: %.*s\n", event->topic_len, event->topic);
+            ESP_LOGV(TAG, "Data:  %.*s\n", event->data_len, event->data);
+
+            char incoming_message[50]; //FIXME: check how long is the longest message on AVR!
+
+            memcpy(incoming_message, event->data, event->data_len);
+            incoming_message[event->data_len] = '\0'; //WARNING: maybe add some checks to the data_len
+            xTaskNotifyGive(i2c_task_handle);
             break;
         default:
             break;
@@ -38,24 +56,24 @@ static void mqtt_init(void) {
             .address.uri = "mqtt://yourmqttserver",
             .address.port = 1883
         },
+    };
 
         esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
         mqttClient = client;
-        /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+        // the last argument may be used to pass data to the event handler
         esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
         esp_mqtt_client_start(client);
 }
 
-static void mqtt_subscribe(void) {
-    msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-}
-
-static void mqtt_publish(void) {}
+void mqtt_publish(void) {}
 
 void mqttTask(void *pvParameters) {
     ESP_LOGI(TAG, "Task started");
 
+    i2c_task_handle = xTaskGetCurrentTaskHandle();
+
     mqtt_init();
+
     ESP_ERROR_CHECK(esp_task_wdt_add(NULL));
 
     for(;;) {
