5dff5da328e985a3b125778c190b3d5445606cee
Erick Ahmed <engineering@erickahmed.com>
2025-12-13 12:40:22 +0100

    Fix initialization of master

--- firmware/esp32/main/i2c.cpp
+++ firmware/esp32/main/i2c.cpp
@@ -36,15 +36,14 @@ static void register_slave(uint8_t addr) {
 }
 
 static void i2c_init(void) {
-    i2c_master_bus_config_t i2c_mst_config = {
-    .clk_source = I2C_CLK_SRC_DEFAULT,
-    .i2c_port = -1,
-    .scl_io_num = I2C_MASTER_SCL_IO,
-    .sda_io_num = I2C_MASTER_SDA_IO,
-    .glitch_ignore_cnt = 7,
-    .flags.enable_internal_pullup = true,
-    };
-
+    i2c_master_bus_config_t i2c_mst_config = {};
+    i2c_mst_config.i2c_port = I2C_NUM_0;
+    i2c_mst_config.scl_io_num = I2C_MASTER_SCL_IO;
+    i2c_mst_config.sda_io_num = I2C_MASTER_SDA_IO;
+    i2c_mst_config.clk_source = I2C_CLK_SRC_DEFAULT;
+    i2c_mst_config.glitch_ignore_cnt = 7;
+    i2c_mst_config.intr_priority = 1;
+    i2c_mst_config.trans_queue_depth = 4;
     ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
 
     // Add all vases adresses here:
